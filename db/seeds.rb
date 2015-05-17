8b4807a49b3e1a5e183600e7014a3900424da540
App Academy Student <student@appacademy.io>
2015-05-17 01:54:04 -0700

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ Cat.create!(name: 'Ludwig', sex: 'M', birth_date: 88.months.ago, color: 'brown',
 Cat.create!(name: 'Snowball', sex: 'F', birth_date: 88.months.ago, color: 'brown', description: "i found her in the trash", user_id: 2)
 
 CatRentalRequest.create!(cat_id: 1, start_date: 7.days.ago, end_date: 4.days.ago, status: 'APPROVED', user_id: 1)
-CatRentalRequest.create!(cat_id: 1, start_date: 5.days.ago, end_date: 3.days.ago, status: 'DENIED', user_id: 1)
+CatRentalRequest.create!(cat_id: 1, start_date: 3.days.ago, end_date: 3.days.ago, status: 'DENIED', user_id: 1)
 CatRentalRequest.create!(cat_id: 1, start_date: 3.days.ago, end_date: 1.days.ago, status: 'DENIED', user_id: 1)
 CatRentalRequest.create!(cat_id: 2, start_date: 26.days.ago, end_date: 1.days.ago, status: 'APPROVED', user_id: 1)
 
