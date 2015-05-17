54d4e4d4e6fb83dcd41b08c6cdd367bf3c401054
App Academy Student <student@appacademy.io>
2015-05-17 00:13:38 -0700

    Add table styling for kitten show pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 Cat.create!(name: 'Garfield', sex: 'M', birth_date: 47.months.ago, color: 'red', user_id: 1)
-Cat.create!(name: 'Spinkles', sex: 'F', birth_date: 65.months.ago, color: 'calico', user_id: 1)
+Cat.create!(name: 'Spinkles', sex: 'F', birth_date: 65.months.ago, color: 'calico', user_id: 1, description: "not actually a cat. details upon inquiry.")
 Cat.create!(name: 'Ludwig', sex: 'M', birth_date: 88.months.ago, color: 'brown', user_id: 1)
 Cat.create!(name: 'Snowball', sex: 'F', birth_date: 88.months.ago, color: 'brown', description: "i found her in the trash", user_id: 2)
 
-CatRentalRequest.create!(cat_id: 1, start_date: 12.days.ago, end_date: 8.days.ago, status: 'APPROVED', user_id: 1)
-CatRentalRequest.create!(cat_id: 1, start_date: 5.days.ago, end_date: 4.days.ago, status: 'DENIED', user_id: 1)
+CatRentalRequest.create!(cat_id: 1, start_date: 7.days.ago, end_date: 4.days.ago, status: 'APPROVED', user_id: 1)
+CatRentalRequest.create!(cat_id: 1, start_date: 5.days.ago, end_date: 3.days.ago, status: 'DENIED', user_id: 1)
 CatRentalRequest.create!(cat_id: 1, start_date: 3.days.ago, end_date: 1.days.ago, status: 'DENIED', user_id: 1)
 CatRentalRequest.create!(cat_id: 2, start_date: 26.days.ago, end_date: 1.days.ago, status: 'APPROVED', user_id: 1)
 
